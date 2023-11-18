da07de727f3760570a3c5eca88c63803bac1619c
Mathias Staricka <mstaricka@gmail.com>
2023-11-18 12:24:28 -0800

    add empty test for day 1

---
+++ src/test/kotlin/com/staricka/adventofcode2023/days/Day1Test.kt
@@ -0,0 +1,19 @@
+package com.staricka.adventofcode2023.days
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day1Test {
+    private val day = Day1()
+
+    @Test
+    fun part1() {
+        assertEquals("", day.part1(""))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals("", day.part2(""))
+    }
+}
