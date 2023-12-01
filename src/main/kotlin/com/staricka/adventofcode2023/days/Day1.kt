952a97c4e36d56f13a0c2d6da7591046482da08b
Mathias Staricka <mstaricka@gmail.com>
2023-11-30 21:36:00 -0800

    day 1 rough

--- src/main/kotlin/com/staricka/adventofcode2023/days/Day1.kt
+++ src/main/kotlin/com/staricka/adventofcode2023/days/Day1.kt
@@ -1,13 +1,55 @@
 package com.staricka.adventofcode2023.days
 
 import com.staricka.adventofcode2023.framework.Day
+import java.lang.Exception
+import java.lang.NumberFormatException
+import java.util.regex.Pattern
+import kotlin.streams.toList
 
 class Day1: Day {
+    val p = Pattern.compile("[0-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
     override fun part1(input: String): Any? {
-        return input
+        var total = 0
+        input.lines().map {
+            if(!it.isBlank()) {
+                var result = it.chars().toList().filter { it >= '0'.toInt() && it <= '9'.toInt() }.first() - '0'.toInt()
+                result *= 10
+                result += it.chars().toList().reversed().filter { it >= '0'.toInt() && it <= '9'.toInt() }.first() - '0'.toInt()
+                total += result
+            }
+        }
+        return total
+    }
+
+    fun parseNum(input: String): Int {
+        try {
+            return Integer.parseInt(input)
+        } catch (e: NumberFormatException) {
+            return when (input) {
+                "one" -> 1
+                "two" -> 2
+                "three" -> 3
+                "four" -> 4
+                "five" -> 5
+                "six" -> 6
+                "seven" -> 7
+                "eight" -> 8
+                "nine" -> 9
+                else -> throw Exception("invalid")
+            }
+        }
     }
 
     override fun part2(input: String): Any? {
-        return input
+        var total = 0
+        input.lines().map {
+            if(!it.isBlank()) {
+                var results = it.indices.map { i -> p.matcher(it.substring(i)).results().findFirst() }.filter { it.isPresent }.map { it.get() }.toList()
+                var result = parseNum(results.first().group()) * 10
+                result += parseNum(results.last().group())
+                total += result
+            }
+        }
+        return total
     }
 }
--- src/test/kotlin/com/staricka/adventofcode2023/days/Day1Test.kt
+++ src/test/kotlin/com/staricka/adventofcode2023/days/Day1Test.kt
@@ -9,11 +9,24 @@ class Day1Test {
 
     @Test
     fun part1() {
-        assertEquals("", day.part1(""))
+        assertEquals(142, day.part1("""
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        """.trimIndent()))
     }
 
     @Test
     fun part2() {
-        assertEquals("", day.part2(""))
+        assertEquals(281, day.part2("""
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()))
     }
 }
