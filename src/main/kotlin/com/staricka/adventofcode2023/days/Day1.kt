f703b1305106aef3b84530c4850bcf09d86006a7
Mathias Staricka <mstaricka@gmail.com>
2023-11-30 21:47:56 -0800

    day 1 clean up

--- src/main/kotlin/com/staricka/adventofcode2023/days/Day1.kt
+++ src/main/kotlin/com/staricka/adventofcode2023/days/Day1.kt
@@ -4,24 +4,17 @@ import com.staricka.adventofcode2023.framework.Day
 import java.lang.Exception
 import java.lang.NumberFormatException
 import java.util.regex.Pattern
-import kotlin.streams.toList
 
 class Day1: Day {
-    val p = Pattern.compile("[0-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
-    override fun part1(input: String): Any? {
-        var total = 0
-        input.lines().map {
-            if(!it.isBlank()) {
-                var result = it.chars().toList().filter { it >= '0'.toInt() && it <= '9'.toInt() }.first() - '0'.toInt()
-                result *= 10
-                result += it.chars().toList().reversed().filter { it >= '0'.toInt() && it <= '9'.toInt() }.first() - '0'.toInt()
-                total += result
-            }
+    private val numPattern = Pattern.compile("^[0-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
+    override fun part1(input: String): Any {
+        return input.lines().filter(String::isNotBlank).sumOf {
+            val digits = it.toCharArray().filter(Char::isDigit).map { c -> Integer.parseInt(c.toString()) }
+            digits.first() * 10 + digits.last()
         }
-        return total
     }
 
-    fun parseNum(input: String): Int {
+    private fun parseNum(input: String): Int {
         try {
             return Integer.parseInt(input)
         } catch (e: NumberFormatException) {
@@ -40,16 +33,13 @@ class Day1: Day {
         }
     }
 
-    override fun part2(input: String): Any? {
-        var total = 0
-        input.lines().map {
-            if(!it.isBlank()) {
-                var results = it.indices.map { i -> p.matcher(it.substring(i)).results().findFirst() }.filter { it.isPresent }.map { it.get() }.toList()
-                var result = parseNum(results.first().group()) * 10
-                result += parseNum(results.last().group())
-                total += result
-            }
+    override fun part2(input: String): Any {
+        return input.lines().filter(String::isNotBlank).sumOf {
+            val results = it.indices.map { i -> numPattern.matcher(it.substring(i)).results().findFirst() }
+                .filter{ o -> o.isPresent}
+                .map { o -> o.get().group() }
+                .toList()
+            parseNum(results.first()) * 10 + parseNum(results.last())
         }
-        return total
     }
 }
