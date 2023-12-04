b86e4fbf0c432474bca4112edffd4602a4d8a36c
Mathias Staricka <mstaricka@gmail.com>
2023-12-03 21:19:05 -0800

    day 4 cleanup

--- src/main/kotlin/com/staricka/adventofcode2023/days/Day4.kt
+++ src/main/kotlin/com/staricka/adventofcode2023/days/Day4.kt
@@ -1,40 +1,40 @@
 package com.staricka.adventofcode2023.days
 
 import com.staricka.adventofcode2023.framework.Day
+import java.util.regex.Pattern
 import kotlin.math.pow
 
 class Day4: Day {
     data class Card(val winning:List<Int>, val have:List<Int>) {
         fun points(): Int {
-            return 2.0.pow(have.filter { winning.contains(it) }.count() - 1).toInt()
+            return 2.0.pow(winning() - 1).toInt()
         }
 
         fun winning(): Int {
-            return have.filter { winning.contains(it) }.count()
+            return have.count { winning.contains(it) }
         }
 
         companion object {
+            private val spaces = Pattern.compile(" +")
+
             fun fromString(input: String): Card {
                 val nums = input.split(":")[1]
-                val winning = nums.split("|")[0].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
-                val have = nums.split("|")[1].trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+                val winning = nums.split("|")[0].trim().split(spaces).map { it.toInt() }
+                val have = nums.split("|")[1].trim().split(spaces).map { it.toInt() }
                 return Card(winning, have)
             }
         }
     }
 
-    override fun part1(input: String): Any? {
+    override fun part1(input: String): Int {
         return input.lines().filter { it.isNotBlank() }
             .map { Card.fromString(it) }
-            .map { it.points() }
-            .sum()
+            .sumOf { it.points() }
     }
 
-    override fun part2(input: String): Any? {
+    override fun part2(input: String): Int {
         val cards = input.lines().filter { it.isNotBlank() }.map { Card.fromString(it) }
-        var sum = cards.size
-        val counts = IntArray(cards.size)
-        for (i in cards.indices) counts[i] = 1
+        val counts = IntArray(cards.size){1}
         for ((c, card) in cards.withIndex()) {
             for (i in 1..card.winning()) {
                 if (c + i < cards.size) {
