170ca737798e1acc876c8024bb577deddcca8e98
Mathias Staricka <mstaricka@gmail.com>
2023-12-22 14:41:26 -0800

    add benchmark option

--- src/main/kotlin/com/staricka/adventofcode2023/framework/Main.kt
+++ src/main/kotlin/com/staricka/adventofcode2023/framework/Main.kt
@@ -1,6 +1,27 @@
 package com.staricka.adventofcode2023.framework
 
 import kotlin.reflect.full.createInstance
+import kotlin.system.measureTimeMillis
+
+fun instantiateDay(dayNumber: Int): Day? {
+    val day = Class.forName("com.staricka.adventofcode2023.days.Day$dayNumber")?.kotlin
+    return day?.createInstance() as Day
+}
+
+fun benchmark() {
+    var total = 0L
+    for (d in 1..25) {
+        val day = instantiateDay(d)
+        if (day != null) {
+            val input = day.inputProvider.getInput(d)
+            val part1 = measureTimeMillis { day.part1(input) }
+            val part2 = measureTimeMillis { day.part2(input) }
+            println("Day $d: ${part1}ms + ${part2}ms = ${part1 + part2}ms")
+            total += part1 + part2
+        }
+    }
+    println("Total: ${total}ms")
+}
 
 fun main() {
     val input = System.getenv("DAY") ?: run {
@@ -10,6 +31,11 @@ fun main() {
 
     if (input.isBlank()) {
         println("Invalid day")
+        return
+    }
+
+    if (input == "BENCHMARK") {
+        benchmark()
         return
     }
 
@@ -27,14 +53,14 @@ fun main() {
     }
 
     try {
-        val day = Class.forName("com.staricka.adventofcode2023.days.Day$dayNumber")?.kotlin
+        val day = instantiateDay(dayNumber)
 
         if (day == null) {
             println("Invalid day")
             return
         }
 
-        (day.createInstance() as Day).run(part)
+        day.run(part)
     } catch (_: ClassNotFoundException) {
         println("Invalid day")
     }
